e37a99247204985a711987fea1d564110155fa0d
leop <leopetit200@gmail.com>
2025-01-12 13:11:50 +0100

    removed subdir

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function (config) {
       reporters: [
         { type: 'html' },
         { type: 'text-summary' },
-        { type: 'lcov', subdir: 'lcov-report' }
+        { type: 'lcov'}
       ]
     },
     reporters: ['progress', 'kjhtml'],
